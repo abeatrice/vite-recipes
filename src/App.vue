41880b1e75d7ba6ff36f6ab5ecbaf979bec9fc5b
andrew beatrice <abeatrice.mail@gmail.com>
2021-01-19 16:54:59 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <recipe-list />
+  </div>
+</template>
+
+<script setup>
+import RecipeList from './components/RecipeList.vue'
+</script>
---
+++ src/components/RecipeList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>recipes</h1>
+    <ul>
+      <li v-for="recipe in recipes" :key="recipe.ID">
+        {{recipe.Name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      recipes: [],
+    }
+  },
+  mounted() {
+    axios.get("http://127.0.0.1:3000/recipes")
+      .then(response => {
+        this.recipes = response.data.data
+      })
+  }
+}
+</script>
